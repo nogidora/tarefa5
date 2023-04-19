d71d821e2bf55a97bbdd980e1e72fd4b9ad4d46b
Giordano Rossa <32661099+nogidora@users.noreply.github.com>
2023-04-19 19:05:47 -0300

    Update code

--- Diff_Eq.vhd
+++ Diff_Eq.vhd
@@ -5,12 +5,11 @@ use ieee.numeric_std.all;
 entity Diff_Eq is
    port(
          x, u, y, dx : in  std_logic_vector (7 downto 0);
-			x_out, u_out, y_out : out std_logic_vector (7 downto 0);
-			S1_out, S2_out, S3_out, S4_out : out std_logic_vector (7 downto 0); 
-			A_OUT, B_OUT, C_OUT, BNEG_OUT, CNEG_OUT : out std_logic_vector (7 downto 0);
-			M1_OUT, M2_OUT, M3_OUT, M4_OUT, M5_OUT : out std_logic_vector (15 downto 0);
-			cont_out    : out integer;
-		   a           : in integer;
+		 a           : in  std_logic_vector (7 downto 0);
+         x_out, u_out, y_out : out std_logic_vector (7 downto 0);
+	     S1_out, S2_out, S3_out, S4_out : out std_logic_vector (7 downto 0); 
+	     A_OUT, B_OUT, C_OUT, BNEG_OUT, CNEG_OUT : out std_logic_vector (7 downto 0);
+		 M1_OUT, M2_OUT, M3_OUT, M4_OUT, M5_OUT : out std_logic_vector (15 downto 0);
          clock       : in  std_logic;
          load        : in  std_logic;
          y_final     : out std_logic_vector (7 downto 0)
@@ -24,18 +23,18 @@ architecture arq_Diff_Eq of Diff_Eq is
 -----COMPONENTS------
 ---------------------
 	component somador_m_bits is
-		GENERIC (M : integer := 8);
+		GENERIC (Z : integer := 8);
 		port (
-				a, b        : in  std_logic_vector(M-1 downto 0);
+				a, b        : in  std_logic_vector(Z-1 downto 0);
 				c0, clock   : in  std_logic;
-				s           : out std_logic_vector(M-1 downto 0);
+				s           : out std_logic_vector(Z-1 downto 0);
 				cout, overf : out std_logic
 			 );
 	end component;
 --
-	component Mult_Sequencial
+	component Mult_Booth is -- MUDAR PARA BOOTH
 		GENERIC (N : integer := 16; -- tamanho do produto
-				   M : integer := 8);
+				 M : integer := 8);
 		port (
 			MD, MR        : in std_logic_vector(M-1 downto 0);
 			clock         : in std_logic;
@@ -53,10 +52,6 @@ architecture arq_Diff_Eq of Diff_Eq is
 	signal s_u : std_logic_vector (7 downto 0);
 	signal s_y : std_logic_vector (7 downto 0);
 
-	signal s_STAGE : integer;
-	
-	signal contador : integer;
-
 	signal s_S1 : std_logic_vector (7 downto 0); -- soma1, soma2, etc...
 	signal s_S2 : std_logic_vector (7 downto 0);
 	signal s_S3 : std_logic_vector (7 downto 0);
@@ -67,8 +62,9 @@ architecture arq_Diff_Eq of Diff_Eq is
 	signal s_C  : std_logic_vector (15 downto 0);
 
 	
+
 	signal Bnegado : std_logic_vector (7 downto 0);
-	signal Cnegado : std_logic_vector (7 downto 0);
+	signal Cnegado : std_logic_vector (7 downto 0);	
 	
 	signal s_M1 : std_logic_vector (15 downto 0); -- mult1, mult2, etc...
 	signal s_M2 : std_logic_vector (15 downto 0);
@@ -83,58 +79,53 @@ architecture arq_Diff_Eq of Diff_Eq is
 -------ARCH--------
 -------------------
 begin
-	STAGE1: process (clock, load, contador) 
+	process (clock, load, s_x) 
 	begin
 		if (clock'event AND clock = '1' AND load = '1') then
 			s_x <= x;  -- so carrega no primeiro ciclo
 			s_y <= y;
 			s_u <= u;
-			contador <= to_integer(unsigned(s_x));
+            
 		else 	
-			if (rising_edge(clock)  AND (contador < a)) then				
-			
-			   Bnegado <= not (s_B (7 downto 0));
-				Cnegado <= not (s_C (7 downto 0));
+			if (rising_edge(clock)  AND (s_x < a)) then				
+			    Bnegado <= not(s_B (7 downto 0));
+                Cnegado <= not(s_C (7 downto 0));
 				
-				s_x1 <= s_S1 (7 downto 0);
-			   s_u1 <= s_S3 (7 downto 0);
+
+                s_x1 <= s_S1 (7 downto 0);
+			    s_u1 <= s_S3 (7 downto 0);
 				s_y1 <= s_S4 (7 downto 0);
 						
-				s_x <= s_x1;
-				s_u <= s_u1;
-				s_y <= s_y1;
-				
-				
-				
-				
-				
-				
-			--- para debugar	
-				x_out <= s_x;
-				y_out <= s_y;
-			   u_out <= s_u;
+				--- para debugar	
 				
 				S1_out <= s_S1;
 				S2_out <= s_S2;
 				S3_out <= s_S3;
-			   S4_out <= s_S4;
-				A_OUT  <=s_A(7 downto 0);
-				B_OUT <= s_B(7 downto 0);
+			    S4_out <= s_S4;
+				
+                
+				B_OUT  <= s_B (7 downto 0);
 				BNEG_OUT <= Bnegado (7 downto 0);
 				
 				C_OUT <= s_C(7 downto 0);
-				BNEG_OUT <= Cnegado;
+				CNEG_OUT <= Cnegado (7 downto 0);
 			
 				M1_OUT <= s_M1;
 				M2_OUT <= s_M2;
 				M3_OUT <= s_M3;
 				M4_OUT <= s_M4;
 				M5_OUT <= s_M5;
-				-------
-				
-				contador <= to_integer (unsigned (s_x));
-				cont_out <= contador;
+	
+			-------
+				s_x <= s_x1;
+				s_u <= s_u1;
+				s_y <= s_y1;				
 				y_final <= s_y (7 downto 0);
+                
+                x_out <= s_x;
+				y_out <= s_y;
+			    u_out <= s_u;
+				
 			end if;
 		end if;
  
@@ -195,7 +186,7 @@ begin
 	
 
 	-- 3 * dx 
-	M1: Mult_Sequencial port map(
+	M1: Mult_Booth port map(
 							"00000011", dx(7 downto 0),
 							clock,
 							load,
@@ -205,7 +196,7 @@ begin
 							);
 	
 	-- s_u * s_x
-	M2: Mult_Sequencial port map(
+	M2: Mult_Booth port map(
 							s_x(7 downto 0), s_u (7 downto 0),
 							clock,
 							load,
@@ -215,7 +206,7 @@ begin
 							);
 							
 	-- s_B = M3 = M1 * M2
-	M3: Mult_Sequencial port map(
+	M3: Mult_Booth port map(
 							s_M1(7 downto 0), s_M2(7 downto 0),
 							clock,
 							load,
@@ -225,7 +216,7 @@ begin
 							);
 
     -- s_C = s_M1 * s_y
-	M4: Mult_Sequencial port map(
+	M4: Mult_Booth port map(
 							s_M1(7 downto 0), s_y (7 downto 0),
 							clock,
 							load,
@@ -235,7 +226,7 @@ begin
 							);
 				
 	-- s_u * dx
-	M5: Mult_Sequencial port map(
+	M5: Mult_Booth port map(
 								s_u(7 downto 0), dx(7 downto 0),
 								clock,
 								load,
